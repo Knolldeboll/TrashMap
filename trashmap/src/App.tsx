429541d785e8127f6638e4cd4acc64aa4a5c41c9
Knolldeboll <chrissi.ruetz@freenet.de>
2025-09-18 17:17:34 +0200

    tailwindcss setup and test

--- trashmap/src/App.tsx
+++ trashmap/src/App.tsx
@@ -2,6 +2,7 @@ import "leaflet/dist/leaflet.css";
 import "./App.css";
 import SimpleMap from "./components/SimpleMap";
 import LibreMapComponent from "./components/LibreMapComponent";
+import tailwindcss from "@tailwindcss/vite";
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -9,17 +10,17 @@ function App() {
   //var map = L.map('map').setView([51.505, -0.09], 13);
   //return <SimpleMap />;
   return (
-
     <>
-
       <div id="center">
         <LibreMapComponent />
       </div>
 
-
+      <div className="flex items-center justify-items-center flex-col">
+        <p>test1</p>
+        <p>test2</p>
+      </div>
     </>
-
-  )
+  );
 }
 
 export default App;
