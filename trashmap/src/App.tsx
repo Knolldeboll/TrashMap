6c150eda528f81ed0f4a26cdf47a925ccbe0bfdc
Knolldeboll <chrissi.ruetz@freenet.de>
2025-09-21 20:51:58 +0200

    Bisschen Tailwind ausprobier Zeug

--- trashmap/src/App.tsx
+++ trashmap/src/App.tsx
@@ -3,24 +3,23 @@ import "./App.css";
 import SimpleMap from "./components/SimpleMap";
 import LibreMapComponent from "./components/LibreMapComponent";
 import tailwindcss from "@tailwindcss/vite";
-
+import TailwindTestComponent from "./components/tailwindtestcomponent";
 function App() {
   //const [count, setCount] = useState(0)
 
   //var map = L.map('map').setView([51.505, -0.09], 13);
   //return <SimpleMap />;
+
+  /*
   return (
     <>
       <div id="center">
         <LibreMapComponent />
       </div>
-
-      <div className="flex items-center justify-items-center flex-col">
-        <p>test1</p>
-        <p>test2</p>
-      </div>
     </>
   );
-}
 
+  */
+  return <TailwindTestComponent></TailwindTestComponent>;
+}
 export default App;
---
+++ trashmap/src/components/TailwindTestComponent.tsx
@@ -0,0 +1,9 @@
+const TailwindTestComponent = () => {
+  return (
+    <div className="bg-amber-200 h-30   text-4xl xl:text-6xl align-text-bottom">
+      testest
+    </div>
+  );
+};
+
+export default TailwindTestComponent;
