c95964f2ff57629ca17a02f56a74e74ad4bd4580
Knolldeboll <chrissi.ruetz@freenet.de>
2025-10-08 23:13:45 +0200

    Support des Marker-Postens implementiert, passiert bei Klick auf rechten Button

--- trashmap/src/components/MarkTrashControl.tsx
+++ trashmap/src/components/MarkTrashControl.tsx
@@ -1,25 +1,53 @@
+import { useMarkerStore } from "../stores/MarkerStore";
+import type { MarkerData } from "../types";
+import { fetchAllMarkers, postSingleMarker } from "../utils/NetworkUtils";
+
 const MarkTrashControl = () => {
+  const setAllMarkers = useMarkerStore((state) => state.setAllMarkers);
 
+  const sendTrashLocation = async () => {
+    const mockMarker: MarkerData = {
+      latitude: Math.random() * 10,
+      longitude: Math.random() * 10,
+      datetime: "08-10-2025 15:56",
+      username: "Grobian",
+    };
+    // TODO: Triggere Methode von tatsächlichem Businesslogik-Component welcher für Netzwerk zuständig ist.
+    console.log("Sending Trash Location:", mockMarker);
 
-    const sendTrashLocation = () => {
+    // Try Posting
+    try {
+      const postresponse = await postSingleMarker(mockMarker);
 
-        // TODO: Triggere Methode von tatsächlichem Businesslogik-Component welcher für Netzwerk zuständig ist.
-
-        console.log("Trash Location sent!")
+      // Try refreshing data
+      try {
+        const data = await fetchAllMarkers();
+        setAllMarkers(data);
+      } catch (err) {
+        console.log("Error while refreshing data after post:", err);
+      }
+    } catch (err) {
+      console.error("Error while sending marker: ", err);
     }
+  };
 
+  return (
+    <>
+      <div
+        id="trashsettingswheel"
+        className="w-1/2 h-full border-r-[1px] border-gray-200"
+      >
+        wie viel Müll
+      </div>
 
-    return <>
-        <div id="trashsettingswheel" className="w-1/2 h-full border-r-[1px] border-gray-200">
-            wie viel Müll
-
-        </div>
-
-        <div id="trashmarkbutton" className="w-1/2 h-full border-l-[1px] border-gray-200 " onClick={sendTrashLocation}>
-            Markiere Müll
-        </div>
+      <div
+        id="trashmarkbutton"
+        className="w-1/2 h-full border-l-[1px] border-gray-200 "
+        onClick={sendTrashLocation}
+      >
+        Markiere Müll
+      </div>
     </>
-
-
-
-}; export default MarkTrashControl;
+  );
+};
+export default MarkTrashControl;
--- trashmap/src/utils/NetworkUtils.ts
+++ trashmap/src/utils/NetworkUtils.ts
@@ -1,38 +1,62 @@
-
 // Sammlung von Methoden für Netzwerk-Kommunikation (über REST)
 
 import type { MarkerData } from "../types";
 
-const VITE_MARKER_URL = import.meta.env.VITE_BACKEND_MARKER_API;
+const VITE_MARKER_URL = import.meta.env.VITE_BACKEND_API_ALL_MARKERS;
+const VITE_POST_MARKER_URL = import.meta.env.VITE_BACKEND_API_SINGLE_MARKER;
 
 // TODO: durch Custom-Hooks ersetzen, wenn weitere Funktionalität wie z.b. das aufrechterhalten von States notwendig wird!
 // z.B. wenn man auch das Handling von Errors etc. auslagern will und diese Sachen nen stabilen State benötigen
 
 export const fetchAllMarkers = async (): Promise<MarkerData[]> => {
+  // TODO: Richtige URL rein.
 
-    // TODO: Richtige URL rein. 
+  console.log("Fetch all Markers");
 
-    console.log("Fetch all Markers")
-    
-    // Der hier wirft automatisch nen Error, wenn z.B. die Adresse nicht aufgelöst werden kann! 
-    // Das untere response.ok ist also ggf. unnötig
-    const response = await fetch(`${VITE_MARKER_URL}`);
+  // Der hier wirft automatisch nen Error, wenn z.B. die Adresse nicht aufgelöst werden kann!
+  // Das untere response.ok ist also ggf. unnötig
+  const response = await fetch(`${VITE_MARKER_URL}`);
 
-    console.log("response", response)
+  console.log("response", response);
 
-    //.ok ist, wenn übertragung gepasst hat, technisch gesehen!
-    if (!response.ok) {
-        //throw error in async: Returne das Promise, aber rejected!
-        console.log("networkutils response not ok!")
-        throw new Error(`Error beim Laden der Ressourcen: ${response.status}`);
+  //.ok ist, wenn übertragung gepasst hat, technisch gesehen!
+  if (!response.ok) {
+    //throw error in async: Returne das Promise, aber rejected!
+    console.log("networkutils response not ok!");
+    throw new Error(`Error beim Laden der Ressourcen: ${response.status}`);
+  }
 
-    }
+  // hier werden die aus dem Json gewonnenen Objekte direkt in ne Liste vom Typ MarkerData[] gemappt!
+  // Das kann man ez machen, solange es nur DTOs ohne Methoden sind!
+  const data: MarkerData[] = await response.json();
 
+  // Return bedeutet in async: Pack den return-Wert in ein Promise bzw. Resolve das Promise mit dem Wert als Value
+  return data;
+};
 
-    // hier werden die aus dem Json gewonnenen Objekte direkt in ne Liste vom Typ MarkerData[] gemappt! 
-    // Das kann man ez machen, solange es nur DTOs ohne Methoden sind!
-    const data: MarkerData[] = await response.json();
+export const postSingleMarker = async (marker: MarkerData) => {
+  console.log("Post single Marker");
+  const init = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      latitude: marker.latitude,
+      longitude: marker.longitude,
+      datetime: marker.datetime,
+      username: marker.username,
+    }),
+  };
 
-    // Return bedeutet in async: Pack den return-Wert in ein Promise bzw. Resolve das Promise mit dem Wert als Value
-    return data;
-}
+  console.log("init: " + init.body);
+  const response = await fetch(`${VITE_POST_MARKER_URL}`, init);
+
+  if (!response.ok) {
+    console.error("POST response not ok: ");
+    throw new Error(`Error beim Posten des Markers: ${response.status}`);
+  }
+
+  console.log("response ok:", response);
+  return response;
+};
